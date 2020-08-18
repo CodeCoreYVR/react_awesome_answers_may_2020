76a6173fc6507a4bf2572f622b7e065c0c955fe2
Brandon Y Lam <me@brandonlam.ca>
2020-08-18 11:31:35 -0700

    refactored onClick event

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -10,6 +10,23 @@ class QuestionIndexPage extends Component {
     }
   }
 
+  // the id within this method is the id of the question that was clicked
+  handleDeleteButtonClick(event, id) {
+    this.setState((currentState) => {
+      // IMPORTANT: In React never mutate any objects (this includes arrays)
+      // If you need to work on a object make a copy... and then work on the copy
+      const questionsCopy = [...currentState.questions]; // spread syntax to copy an array (SHALLOW COPY)
+      // const questionsCopy = [].concat(currentState.questions); // concat method to copy an array (SHALLOW COPY)
+      // const questionsCopy = JSON.parse(JSON.stringify(currentState.questions)); // DEEP COPY with JSON.stringify But this cannot copy values like functions or `undefined` or `null`
+      const updatedArray = questionsCopy.filter((q) => {
+        return q.id !== id
+      })
+      return {
+        questions: updatedArray
+      }
+    })
+  }
+
   render() {
     return(
       <main id='question-index-page'>
@@ -18,13 +35,7 @@ class QuestionIndexPage extends Component {
           {this.state.questions.map((question) => {
             return <li key={question.id}>
               <a href='#'>{question.title}</a>
-              <button onClick={(event) => {
-                this.setState((currentState) => {
-                  return {
-                    questions: []
-                  }
-                })
-              }}>
+              <button onClick={ (event) => this.handleDeleteButtonClick(event, question.id) }>
                 Delete
               </button>
             </li>
