7b228e0fd19d1884a0cf3031266b92b5d2291863
Brandon Y Lam <me@brandonlam.ca>
2020-08-25 11:21:43 -0700

    add AuthRoute

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
 } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SignInPage from './components/SignInPage';
+import AuthRoute from './components/AuthRoute';
 
 class App extends Component {
   constructor(props) {
@@ -54,7 +55,12 @@ class App extends Component {
         <Switch>
           {/* When creating routes: make sure the higher specificity routes sit above lower specificity routes */}
           <Route path='/questions' exact={true} component={QuestionIndexPage}/>
-          <Route path='/questions/new' component={QuestionCreatePage} />
+          {/* <Route path='/questions/new' component={QuestionCreatePage} /> */}
+          <AuthRoute 
+            path='/questions/new'
+            isAuthenticated={!!this.state.user}
+            component={QuestionCreatePage}
+          />
           <Route path='/questions/:id' component={QuestionPage} />
           {/* <Route path='/sign_in' component={SignInPage} /> */}
           <Route path='/sign_in'
